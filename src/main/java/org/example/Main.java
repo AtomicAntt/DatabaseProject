360a86933c5ca2688b7d00701b9913cf63c01d53
AtomicAntt <anthonychi36@gmail.com>
2024-11-29 23:24:33 -0800

    Allow user to add/remove from songs and albums as well as display all tables in the database

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -46,16 +46,22 @@ public class Main {
                         addMusician(conn, scanner);
                         break;
                     case 2:
+                        removeMusician(conn, scanner);
                         break;
                     case 3:
+                        addAlbum(conn, scanner);
                         break;
                     case 4:
+                        removeAlbum(conn, scanner);
                         break;
                     case 5:
+                        addSong(conn, scanner);
                         break;
                     case 6:
+                        removeSong(conn, scanner);
                         break;
                     case 7:
+                        displayInformation(conn);
                         break;
                     default:
                         System.out.println("Invalid choice");
@@ -101,9 +107,120 @@ public class Main {
             insertMusicianStatement.setString(3, phoneNum);
             insertMusicianStatement.setString(4, address);
             insertMusicianStatement.executeUpdate();
-            System.out.println("Musician with SSN " + ssn + ", name " + name + ", phone # " + phoneNum + ", and lives at " + address + " has been added to the database!");
+            System.out.println("Musician successfully added!");
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public static void removeMusician(Connection connection, Scanner scanner){
+        System.out.println("Enter the musician's SSN to remove them:");
+        String ssn = scanner.nextLine();
+
+        String query = "DELETE FROM Musician WHERE SSN = ?";
+        try (PreparedStatement statement = connection.prepareStatement(query)){
+            statement.setString(1, ssn);
+            statement.executeUpdate();
+            System.out.println("Removed the musician with SSN: " + ssn);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void addAlbum(Connection connection, Scanner scanner){
+        System.out.println("Enter album title:");
+        String title = scanner.nextLine();
+        System.out.println("Enter copyright date (YYYY-MM-DD):");
+        String copyrightDate = scanner.nextLine();
+        System.out.println("Enter album format:");
+        String format = scanner.nextLine();
+        System.out.println("Enter album identifier (integer):");
+        int albumIdentifier = scanner.nextInt();
+
+        // Insert the album
+        String insertQuery = "INSERT INTO Album (Title, CopyrightDate, Format, AlbumIdentifier) VALUES (?, ?, ?, ?)";
+        try (PreparedStatement insertStatement = connection.prepareStatement(insertQuery)){
+            insertStatement.setString(1, title);
+            insertStatement.setDate(2, Date.valueOf(copyrightDate));
+            insertStatement.setString(3, format);
+            insertStatement.setInt(4, albumIdentifier);
+            insertStatement.executeUpdate();
+            System.out.println("Album added!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void removeAlbum(Connection connection, Scanner scanner){
+        System.out.println("Enter the ID of the album you want to remove:");
+        int id = scanner.nextInt();
+
+        String query = "DELETE FROM Album WHERE AlbumID = ?";
+        try (PreparedStatement statement = connection.prepareStatement(query)){
+            statement.setInt(1, id);
+            statement.executeUpdate();
+            System.out.println("Removed the album with ID: " + id );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void addSong(Connection connection, Scanner scanner){
+        System.out.println("Enter song title:");
+        String title = scanner.nextLine();
+        System.out.println("Enter song author:");
+        String author = scanner.nextLine();
+
+        // Insert the album
+        String insertQuery = "INSERT INTO Song (Title, Author) VALUES (?, ?)";
+        try (PreparedStatement insertStatement = connection.prepareStatement(insertQuery)){
+            insertStatement.setString(1, title);
+            insertStatement.setString(2, author);
+            insertStatement.executeUpdate();
+            System.out.println("Song added!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void removeSong(Connection connection, Scanner scanner){
+        System.out.println("Enter the ID of the song you want to remove:");
+        int id = scanner.nextInt();
+
+        String query = "DELETE FROM Song WHERE SongID = ?";
+        try (PreparedStatement statement = connection.prepareStatement(query)){
+            statement.setInt(1, id);
+            statement.executeUpdate();
+            System.out.println("Removed the song with ID: " + id );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void displayInformation(Connection connection) throws SQLException {
+        String[] tables = {"Home", "Musician", "Instrument", "Album", "Song", "MusicianInstrument", "SongPerformance"};
+        for (String table : tables) {
+            System.out.println("\nTable: " + table);
+            String query = "SELECT * FROM " + table;
+            try (Statement stmt = connection.createStatement();
+                 ResultSet rs = stmt.executeQuery(query)) {
+                ResultSetMetaData metaData = rs.getMetaData();
+                int columnCount = metaData.getColumnCount();
+
+                // Print column headers
+                for (int i = 1; i <= columnCount; i++) {
+                    System.out.print(metaData.getColumnName(i) + "\t|\t");
+                }
+                System.out.println();
+
+                // Print rows
+                while (rs.next()) {
+                    for (int i = 1; i <= columnCount; i++) {
+                        System.out.print(rs.getString(i) + "\t|\t");
+                    }
+                    System.out.println();
+                }
+            }
         }
     }
 }
